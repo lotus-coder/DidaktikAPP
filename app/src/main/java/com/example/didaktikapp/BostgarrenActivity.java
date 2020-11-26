2d941ee410f28e6535e3eefafd38993e921c3fe3
Cesferort <cesferort99@gmail.com>
2020-11-26 09:49:21 +0100

    26 NOV 9:49 César

--- app/src/main/java/com/example/didaktikapp/BostgarrenActivity.java
+++ app/src/main/java/com/example/didaktikapp/BostgarrenActivity.java
@@ -6,37 +6,30 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.MediaController;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 public class BostgarrenActivity extends AppCompatActivity
 {
     private VideoView videoView;
     private MediaController mediaController;
+    private TextView tvBostgarren;
 
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bostgarren);
 
+        //Preparar el video view
         videoView = (VideoView)findViewById(R.id.bostgarren_videoView);
         mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
 
-        iniciarVideo();
+        tvBostgarren = (TextView)findViewById(R.id.tvBostgarren);
+        tvBostgarren.setText("Kostaldeko lurralde gehienetan bezala, Lekeitioko lurrak ere inoiz ez dira izan nekazaritza lantzeko lur onak. Horren ondorioz, laboreak errotetara eraman eta han lantzen zituzten. Baina, XVI. mendean lehorte-garai bat egon zen, eta horren ondorioz erreketan kokatuta zeuden errotek ez zuten funtzionatzen. Hortik, bururatu zitzaien errota hau eraikitzeko ideia; izan ere, errota hau marea-errota bat da. Hau da, marearen gorabeherekin funtzionatzen duen errota, horregatik errotak funtzionatzeko behar duen ura egongo dela bermatuta dago. Marea-errota hau eraikitzea garai hartan oso esanguratsua zen, zaila zelako eraikitzea eta gainera, marea-erroten ideia Europatik zabaldu zelako gurera. Horrez gain, leku horretan zehar ontziak ere ibili ahal ziren, errotarekin zuten komunikazioa ahalbidetuz. Azkenik, aipagarria da gaur egun Euskal Herrian oso gutxi aurkitzen direla.  ");
+
         eventos();
-    }
-
-    private void iniciarVideo()
-    {
-        //specify the location of media file
-        Uri uri= Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.aurkezpena);
-
-        //Setting MediaController and URI, then starting the videoView
-        videoView.setMediaController(mediaController);
-        videoView.setVideoURI(uri);
-        videoView.requestFocus();
-
-        videoView.start();
+        iniciarVideoMarierrota();
     }
 
     private void eventos()
@@ -45,11 +38,17 @@ public class BostgarrenActivity extends AppCompatActivity
         {
             public void onCompletion(MediaPlayer mp)
             {
-                String url = "https://www.youtubetrimmer.com/view/?v=VyGwC4gH-DA&end=194";
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                BostgarrenActivity.this.startActivityForResult(intent,1);
+                Intent intent = new Intent(BostgarrenActivity.this, BostgarrenGalderak.class);
+                startActivity(intent);
             }
         });
+    }
+
+    private void iniciarVideoMarierrota()
+    {
+        String url = "https://www.youtubetrimmer.com/view/?v=VyGwC4gH-DA&end=194";
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        BostgarrenActivity.this.startActivityForResult(intent,1);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
@@ -57,18 +56,16 @@ public class BostgarrenActivity extends AppCompatActivity
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == 1 && resultCode == RESULT_OK )
-        {
-            MediaPlayer mp=new MediaPlayer();
-            try{
-                mp.setDataSource("/sdcard/Music/maine.mp3");//Write your location here
-                mp.prepare();
-                mp.start();
+            iniciarAudioMarierrota();
+    }
 
-            }
-            catch(Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
+    private void iniciarAudioMarierrota()
+    {
+        Uri uri= Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.marierrota_audioa);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+
+        videoView.start();
     }
 }
---
+++ app/src/main/java/com/example/didaktikapp/BostgarrenGalderak.java
@@ -0,0 +1,15 @@
+package com.example.didaktikapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BostgarrenGalderak extends AppCompatActivity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bostgarren_galderak);
+
+    }
+}
